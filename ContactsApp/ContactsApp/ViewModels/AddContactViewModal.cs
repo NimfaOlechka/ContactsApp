d5e62dc3919fd2d5f1e27e4d9589eadb509f9c6a
Olga <nimfaolya@mail.ru>
2019-12-10 22:31:30 +0100

    added viewmodels
    - added controller for list of contacts  - populate manually data
    - controller for add contact - in progress

---
+++ ContactsApp/ContactsApp/ViewModels/AddContactViewModal.cs
@@ -0,0 +1,31 @@
+﻿using ContactsApp.Models;
+using ContactsApp.Views;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using Xamarin.Forms;
+using System.ComponentModel;
+using System.Threading.Tasks;
+
+namespace ContactsApp.ViewModels
+{
+    class AddContactViewModal :INotifyPropertyChanged
+    {
+        public Command SaveCommand { get; set; }
+        public Command CancelCommand { get; set; }
+
+        public Contact Contact { get; set; }
+        
+        public AddContactViewModal()
+        {
+            Contact = new Contact
+            {
+                PhoneNumber = "Phoone number",
+                Name = "Coontact Name"
+            };
+           
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ ContactsApp/ContactsApp/ViewModels/ContactsViewModel.cs
@@ -0,0 +1,41 @@
+﻿using ContactsApp.Models;
+using ContactsApp.Views;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using Xamarin.Forms;
+using System.ComponentModel;
+using System.Threading.Tasks;
+
+namespace ContactsApp.ViewModels
+{
+    class ContactsViewModel: INotifyPropertyChanged
+    {
+        //collection to store all contacts
+        public ObservableCollection<Contact> Contacts { get; set; }
+        //command to load collection
+        public Command AddContactCommand { get; set; }
+        //command to add new contact
+        public ContactsViewModel()
+        {
+           //create some data
+            Contacts = new ObservableCollection<Contact>()
+            {
+                new Contact{ PhoneNumber = "+45 20202020", Name = "Anna"},
+                new Contact{ PhoneNumber = "+45 20342020", Name = "Anton"},
+                new Contact{ PhoneNumber = "+45 20452020", Name = "Elena"},
+                new Contact{ PhoneNumber = "+45 67206720", Name = "Peter"},
+                new Contact{ PhoneNumber = "+45 20456720", Name = "John"}
+            };
+
+            AddContactCommand = new Command(async()=> {
+                var addContacPage = new AddNewContactPage();
+                await Application.Current.MainPage.Navigation.PushAsync(addContacPage);
+            });
+        }
+        
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
