d14be61c0b203886c431b91cd7e0622bb10d5e53
Olga <nimfaolya@mail.ru>
2019-12-10 22:34:02 +0100

    - changed navigation and main page loading sequence

---
+++ ContactsApp/ContactsApp/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using ContactsApp.Views;
+
+namespace ContactsApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage( new ContactsListViewPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
