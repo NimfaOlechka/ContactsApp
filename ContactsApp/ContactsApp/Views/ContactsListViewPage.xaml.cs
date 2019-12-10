e9a7b600e138912f4988fe32e76e1346fce999fb
Olga <nimfaolya@mail.ru>
2019-12-10 22:33:21 +0100

    added views
    - contacts view - collection of contacts
    - add contact view - input fields for creating new contact

---
+++ ContactsApp/ContactsApp/Views/ContactsListViewPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using ContactsApp.Models;
+using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ContactsApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ContactsListViewPage : ContentPage
+    {
+        
+
+        public ContactsListViewPage()
+        {
+            InitializeComponent();
+            
+
+        }
+
+    }
+}
