f551c0d5364eee5cf9d3ec63415d966b629ec30c
Olga <nimfaolya@mail.ru>
2019-12-10 22:29:09 +0100

    added models
    added 2 classes adde
    
    
    
    
    
    
    
    class contact and contactslist (not used yet)

---
+++ ContactsApp/ContactsApp/Models/Contact.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContactsApp.Models
+{
+    public class Contact
+    {
+        public string PhoneNumber { get; set; }
+        public string Name { get; set; }    
+    }
+}
---
+++ ContactsApp/ContactsApp/Models/ContactsListData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContactsApp.Models
+{
+    public class ContactsListData
+    {
+        public List<Contact> contacts;
+
+        public ContactsListData()
+        {
+            contacts = new List<Contact>()
+            {
+                new Contact{ PhoneNumber = "+45 20202020", Name = "Anna"},
+                new Contact{ PhoneNumber = "+45 20342020", Name = "Anton"},
+                new Contact{ PhoneNumber = "+45 20452020", Name = "Elena"},
+                new Contact{ PhoneNumber = "+45 67206720", Name = "Peter"},
+                new Contact{ PhoneNumber = "+45 20456720", Name = "John"}
+            };
+        }
+    }
+}
