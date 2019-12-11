cdd5804f615951d5e32613ea6d56bb36ed65646c
Olga <nimfaolya@mail.ru>
2019-12-11 18:41:34 +0100

    -added interface and class for contacts data
    - added interface with REST service and class to simulate db

--- ContactsApp/ContactsApp/Models/ContactsListData.cs
+++ ContactsApp/ContactsApp/Models/ContactsListData.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace ContactsApp.Models
 {
-    public class ContactsListData
+    public class ContactsListData: IDataStore<Contact>
     {
-        public List<Contact> contacts;
+        readonly List<Contact> contacts;
 
         public ContactsListData()
         {
@@ -18,6 +20,37 @@ namespace ContactsApp.Models
                 new Contact{ PhoneNumber = "+45 67206720", Name = "Peter"},
                 new Contact{ PhoneNumber = "+45 20456720", Name = "John"}
             };
+        }
+
+        public async Task<bool> AddItemAsync(Contact item)
+        {
+            contacts.Add(item);
+            return await Task.FromResult(true);
+        }
+
+        public async Task<bool> DeleteItemAsync(string id)
+        {
+            var contactToDelete = contacts.Where((Contact arg) => arg.PhoneNumber == id).FirstorDefault();
+            contacts.Remove(contactToDelete);
+            return await Task.FromResult(true);
+        }
+
+        public async Task<Contact> GetItemAsync(string id)
+        {
+            return await Task.FromResult(contacts.FirstOrDefault(c => c.PhoneNumber == id || c.Name == id));
+        }
+
+        public async Task<IEnumerable<Contact>> GetItemsAsync(bool forceRefresh = false)
+        {
+            return await Task.FromResult(contacts);
+        }
+
+        public async Task<bool> UpdateItemAsync(Contact item)
+        {
+            var oldItem = contacts.Where((Contact arg) => arg.PhoneNumber == item.PhoneNumber).FirstOrDefault();
+            contacts.Remove(oldItem);
+            contacts.Add(item);
+            return await Task.FromResult(true);
         }
     }
 }
